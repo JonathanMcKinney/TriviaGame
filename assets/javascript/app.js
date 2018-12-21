9a46eeea8c2df8521bfd292d744957e5c58d04e1
J.McKinney <jsmckinney89@gmail.com>
2018-12-21 04:29:45 -0500

    added beginnings of pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,28 @@
+// Create start page
+// - create box with name and start button
+// - On button click, game begins
+//     -display screen disappears and question screen begins, countdown begins
+
+// Quiz screen
+
+// -Create an object for each question 
+//     quesetion = {
+//         Q1: another object {
+//             answer: true (correct answer),
+//             answer1: false (incorrect answer),
+//             answer2: false,
+//             answer3: false,
+//         }
+//         Q2: {
+//             answer: t/f,
+//             ...
+//         }
+//         ...
+//     }
+// -on submit button click or timer = zero the game ends and results screen is displayed
+
+// Result screen
+
+// -if true statement chosen when false, incorrect ++, if true === true correct++
+// pull answer counts and add to screen
+
